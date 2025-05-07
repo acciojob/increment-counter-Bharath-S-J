ddf49c1b6ba46b38faa0e7ac940dd9fd43fb5cad
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-07 16:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn =document.getElementById("incrementBtn")
+const counter=document.getElementById("counter")
+let c=0;
+
+btn.addEventListener('click', ()=>{
+	c++;
+	counter.innerText=c.toString()
+})
